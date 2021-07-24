c7c9271ac7673d96d42619672fee3c187d26686c
ALBERT JEBAKUMAR <61425407+AlbertJeba@users.noreply.github.com>
2021-07-24 23:55:52 +0530

    Phare 2 Google sign on added Date : 24-07-2021 Time : 23:55 pm

--- app/src/main/java/com/example/tsnews/NewsFeed.java
+++ app/src/main/java/com/example/tsnews/NewsFeed.java
@@ -1,12 +1,23 @@
 package com.example.tsnews;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,11 +26,16 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tsnews.databinding.ActivityNewsFeedBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NewsFeed extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityNewsFeedBinding binding;
+    ImageView profile_photo;
+    TextView u_name,u_email;
+    Button logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,40 @@ public class NewsFeed extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarNewsFeed.toolbar);
+        //Stat-off Profile details from Google Sign-in
+        NavigationView navigationView = binding.navView;
+        profile_photo=navigationView.getHeaderView(0).findViewById(R.id.user_profile_photo);
+        u_name=navigationView.getHeaderView(0).findViewById(R.id.user_name);
+        u_email=navigationView.getHeaderView(0).findViewById(R.id.user_email_id);
+        //logout=navigationView.getMenu().findItem(R.id.Logout);
+        navigationView.getMenu().findItem(R.id.Logout).setOnMenuItemClickListener(menuItem -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(getApplicationContext(),SignInActivity.class));
+            // [START auth_fui_signout]
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // ...
+                        }
+                    });
+            finish();
+            return true;
+        });
+        GoogleSignInAccount account= GoogleSignIn.getLastSignedInAccount(this);
+        u_name.setText(account.getDisplayName());
+        u_email.setText(account.getEmail());
+       Glide.with(this).load(account.getPhotoUrl()).into(profile_photo);
+       /* logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),SignInActivity.class));
+                finish();
+            }
+        });
+8?
+        //End-off Profile details from Google Sign-in
         /*binding.appBarNewsFeed.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,7 +87,7 @@ public class NewsFeed extends AppCompatActivity {
             }
         });*/
         DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
---
+++ app/src/main/java/com/example/tsnews/SignInActivity.java
@@ -0,0 +1,109 @@
+package com.example.tsnews;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class SignInActivity extends AppCompatActivity {
+
+    SignInButton btn;
+    GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       // updateUI(currentUser);
+        if (currentUser!=null)
+            startActivity(new Intent(getApplicationContext(),NewsFeed.class));
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        btn=findViewById(R.id.SignInButtonGoogle);
+        mAuth = FirebaseAuth.getInstance();
+        processrequest();
+        
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processlogin();
+            }
+        });
+        
+    }
+
+    private void processrequest() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    private void processlogin() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 101);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 101) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(),"Error is Google's Info ",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(),NewsFeed.class));
+                            finish();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Problem is FireBase ",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+}
